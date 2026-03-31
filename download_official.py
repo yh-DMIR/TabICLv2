2847372e4e4f94c9ec9ab20f2385b96510e6a133
lyh <lyh191127@gmail.com>
2026-03-31 23:43:23 +0800

    1

--- download_official.py
+++ download_official.py
@@ -7,7 +7,7 @@ target_dir = "/vast/users/guangyi.chen/causal_group/zijian.li/dmir_crl/lyh/TabIC
 os.makedirs(target_dir, exist_ok=True)
 
 # 根据你提供的文档截图，精确的 v2 版本文件名
-repo_id = "yh-DMIR/TabICLv2"  # 官方 Hugging Face 仓库名
+repo_id = "jingang/TabICL"  # 官方 Hugging Face 仓库名
 ckpts = [
     "tabicl-classifier-v2-20260212.ckpt",
     "tabicl-regressor-v2-20260212.ckpt"
